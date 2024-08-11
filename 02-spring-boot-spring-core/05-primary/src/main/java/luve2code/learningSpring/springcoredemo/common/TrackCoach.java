3a4d7c48a89c98f3c69ce84ad6f2e9b64cb8f420
Cemal <c.igrek63@hotmail.com>
2024-08-11 21:45:51 +0300

    I Learned Primary

---
+++ 02-spring-boot-spring-core/05-primary/src/main/java/luve2code/learningSpring/springcoredemo/common/TrackCoach.java
@@ -0,0 +1,13 @@
+package luve2code.learningSpring.springcoredemo.common;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+@Primary
+public class TrackCoach implements Coach{
+    @Override
+    public String getDailyWorkout() {
+        return "Run a hard 5k!";
+    }
+}
